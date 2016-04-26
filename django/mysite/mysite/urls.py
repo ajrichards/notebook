83271e91d8b1869c0a181a9b330452c56e9d139d
Adam Richards <adamricha@gmail.com>
2016-04-25 18:47:30 -0600

    added poll app

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
