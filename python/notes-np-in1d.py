1188f8de414505b4b909eb628109f7099700d175
Adam Richards <adamricha@gmail.com>
2018-09-07 08:16:58 -0600

    added some missing

--- python/notes-np-in1d.py
+++ python/notes-np-in1d.py
@@ -8,20 +8,20 @@ b = np.array([3,4,5])
 
 mask1 = np.in1d(a,b)
 mask2 = np.in1d(b,a)
+print('mask1',mask1)
+print('mask2',mask2)
 
 
-
-print mask1
-print np.where(mask1==True)
+#print mask1
+#print np.where(mask1==True)
 sys.exit()
 
-print mask2
+
 
 
 
 mask2Inds = np.where(mask2==True)[0]
 #mask12nds = map(lambda x: 1 if x else 0, mask1)
-
-print mask2Inds
-print b[mask2]
-print b[mask2Inds]
+#print mask2Inds
+#print b[mask2]
+#print b[mask2Inds]
---
+++ system-admin/howto-vpn.rst
@@ -0,0 +1,3 @@
+~$ sudo apt install network-manager-openconnect
+~$ sudo systemctl daemon-reload
+~$ sudo apt-get install lib32z1 lib32ncurses5
